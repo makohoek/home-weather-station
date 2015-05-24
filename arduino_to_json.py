5d8be71d5310282c6d84c219f1181a64d1a7a2a0
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-05-24 15:57:53 +0200

    Core: reimplement arduino interface
    I want it to be usable with the "with" statement in python.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- arduino_to_json.py
+++ arduino_to_json.py
@@ -8,19 +8,26 @@ import json
 
 
 #TODO: reimplement this as a client service design pattern
-#TODO: reimplment this as a with statement stuff
-class TemperatureInterface(object):
+class TemperatureInterface:
 
     def __init__(self):
-        self.connection = serial.Serial("/dev/cu.usbmodem621", 9600)
+        self.__serial_device = "/dev/cu.usbmodem621"
+        self.__baudrate = 9600
         self.requestTemperatureCommandCode = '1'
+
+    def __enter__(self):
+        self.__connection = serial.Serial(self.__serial_device, self.__baudrate)
         # FIXME: for now, we are just sleeping after a new connection
         # we should implement at arduino side a command to check if
         # serial connection is established
         time.sleep(2)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.__connection.close()
 
     def request_new_sample(self):
-        self.connection.write(self.requestTemperatureCommandCode)
+        self.__connection.write(self.requestTemperatureCommandCode)
 
     def wait_for_response(self):
         # FIXME: for now, we are just sleeping after a request
@@ -28,13 +35,13 @@ class TemperatureInterface(object):
         # is available
         time.sleep(0.5)
 
-    def __read_sample(self):
-        temperature = self.connection.readline()
-        return temperature.strip()
-
     def retrieve_new_sample(self):
         sample_with_prefix = self.__read_sample()
         return sample_with_prefix.lstrip('C: ')
+
+    def __read_sample(self):
+        temperature = self.__connection.readline()
+        return temperature.strip()
 
 
 def temperature_sample_to_json(today, now, temperature):
@@ -53,39 +60,39 @@ def get_today_string():
 
 
 if __name__ == '__main__':
-    arduino = TemperatureInterface()
-    data = []
+    with TemperatureInterface() as arduino:
+        data = []
 
-    #TODO: add daystring in filename
-    today_str = get_today_string()
-    current_data_file = 'temperature_' + today_str + '.json'
+        #TODO: add daystring in filename
+        today_str = get_today_string()
+        filename = 'temperature_' + today_str + '.json'
 
-    while True:
-        # get current data
-        with open("thermal_data.json", "r") as datafile:
-            data = json.load(datafile)
-            print data
+        while True:
+            # get current data
+            with open("thermal_data.json", "r") as datafile:
+                data = json.load(datafile)
+                print data
 
-        # request a new sample
-        arduino.request_new_sample()
+            # request a new sample
+            arduino.request_new_sample()
 
-        # wait for the thermometer to process
-        arduino.wait_for_response()
+            # wait for the thermometer to process
+            arduino.wait_for_response()
 
-        # prepare date
-        localtime = time.localtime()
-        today = time.strftime("%Y-%m-%d", localtime)
-        now = time.strftime("%H:%M:%S", localtime)
-        # retrieve the sample
-        sample = arduino.retrieve_new_sample()
+            # prepare date
+            localtime = time.localtime()
+            today = time.strftime("%Y-%m-%d", localtime)
+            now = time.strftime("%H:%M:%S", localtime)
+            # retrieve the sample
+            sample = arduino.retrieve_new_sample()
 
-        # put the sample in json format to the current data
-        data.append(temperature_sample_to_json(today, now, sample))
+            # put the sample in json format to the current data
+            data.append(temperature_sample_to_json(today, now, sample))
 
-        # write it back to the file
-        with open("thermal_data.json", "w") as datafile:
-            parseable_data = json.dumps(data)
-            datafile.write(parseable_data)
+            # write it back to the file
+            with open("thermal_data.json", "w") as datafile:
+                parseable_data = json.dumps(data)
+                datafile.write(parseable_data)
 
-        # wait a minute before requesting a new sample
-        time.sleep(60)
+            # wait a minute before requesting a new sample
+            time.sleep(60)
