58fc3b70e48bc0a4957529f487055e2a497fa64e
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-05-29 13:20:08 +0200

    Misc: introduce a script to generate fake data
    In order to properly test the visualisation part, it can be handy to
    generate a data file which is understandable by our javascript.
    
    This patch adds a script which will generate a fake data file called
    `thermal_data.json`.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

---
+++ generate_data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import pprint
+import random
+import re
+import time
+
+def generate_sample_data():
+    localtime=time.localtime()
+    today=time.strftime("%Y-%m-%d", localtime)
+    now=time.strftime("%H:%M:%S", localtime)
+    temperature=random.randint(18,32)
+    entry={
+        "date": today,
+        "time": now,
+        "temperature": temperature,
+    }
+    return entry
+
+data=[]
+
+for i in range(1,340):
+    entry = generate_sample_data()
+    data.append(entry)
+    print entry
+    time.sleep(1)
+
+with open("thermal_data.json","w") as datafile:
+    # d3js reads with double quotes, whereas
+    # pprint renders the data strings in simple quotes
+    # therefore, we need to substitute ' by "
+    parseable_data=re.sub("\'","\"", pprint.saferepr(data))
+    datafile.write(parseable_data)
