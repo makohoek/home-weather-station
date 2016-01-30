d4f732a03e8eab9e20b8b1e3ceea888d4a95767d
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2016-01-30 12:05:14 +0100

    generate-data: prepare refactor with controller.py
    Logic in generate_data.py and controller.py is quite similar, the only
    thing which changes is what provides the data: a random number or the
    arduino serial port.
    
    This patch aligns generate_data to prepare a refactor of both scripts.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- generate_data.py
+++ generate_data.py
@@ -3,14 +3,11 @@
 import json
 import os.path
 import random
+import shutil
 import time_utils
 from time import sleep
 
-
-def generate_sample_data():
-    today = time_utils.get_today_string()
-    now = time_utils.get_now_string()
-    temperature = random.randint(18, 32)
+def temperature_sample_to_json(today, now, temperature):
     entry = {
         "date": today,
         "time": now,
@@ -22,10 +19,10 @@ if __name__ == "__main__":
     data = []
 
     while True:
-
         today_str = time_utils.get_today_string()
         filename = '_fake_temperature_' + today_str + '.json'
 
+        # create file if it does not exists
         if not os.path.isfile(filename):
             with open(filename, "w") as newfile:
                 newfile.write('[]')
@@ -35,13 +32,21 @@ if __name__ == "__main__":
             data = json.load(datafile)
             print data
 
-        entry = generate_sample_data()
-        data.append(entry)
+        # prepare date
+        today = time_utils.get_today_string()
+        now = time_utils.get_now_string()
+        # generate the sample
+        temperature = random.randint(18, 32)
+
+        # put the sample in json format to the current data
+        data.append(temperature_sample_to_json(today, now, temperature))
 
         # write it back to the file
         with open(filename, "w") as datafile:
             parseable_data = json.dumps(data)
             datafile.write(parseable_data)
+
+        shutil.copyfile(filename, 'visualisation/' + filename)
 
         # wait before requesting a new sample
         sleep(1)
